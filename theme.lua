0da651664aa52f38f37e8492f13673a1c9e9f333
Sole <adcbuyucu@gmail.com>
2026-04-04 08:10:08 +0300

    Update theme.lua

--- theme.lua
+++ theme.lua
@@ -1,11 +1,11 @@
 local theme = {
-    bg_deep = Color3.fromRGB(5, 5, 5),
-    bg_sidebar = Color3.fromRGB(10, 0, 0),
-    accent_main = Color3.fromRGB(136, 0, 0),
-    accent_glow = Color3.fromRGB(255, 0, 0),
+    bg_deep = Color3.fromRGB(5, 5, 5),       -- #050505
+    bg_sidebar = Color3.fromRGB(10, 0, 0),    -- #0a0000
+    bg_element = Color3.fromRGB(17, 17, 17),  -- #111111
+    accent_main = Color3.fromRGB(136, 0, 0),  -- #880000
+    accent_glow = Color3.fromRGB(255, 0, 0),  -- #ff0000
     text_main = Color3.fromRGB(255, 255, 255),
     text_dark = Color3.fromRGB(136, 136, 136),
-    window_size = Vector2.new(620, 390), --
-    sidebar_w = 170 --
+    window_size = UDim2.new(0, 620, 0, 390)   --
 }
 return theme
