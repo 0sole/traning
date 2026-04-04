0b725e1c194d1770dee1b7d2db4ec5f630df4501
Sole <adcbuyucu@gmail.com>
2026-04-04 07:44:11 +0300

    Add theme configuration in theme.lua

---
+++ theme.lua
@@ -0,0 +1,12 @@
+local theme = {
+    bg_deep = {5/255, 5/255, 5/255},       -- #050505
+    bg_sidebar = {10/255, 0, 0},          -- #0a0000
+    accent_main = {136/255, 0, 0},        -- #880000
+    accent_glow = {255/255, 0, 0},        -- #ff0000
+    text_main = {1, 1, 1},
+    text_dark = {136/255, 136/255, 136/255},
+    window_w = 620,
+    window_h = 390,
+    sidebar_w = 170
+}
+return theme
