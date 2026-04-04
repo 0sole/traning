372372bfccc4e2d108c18ce8db4007425fda0550
Sole <adcbuyucu@gmail.com>
2026-04-04 14:00:21 +0300

    Update theme.lua

--- theme.lua
+++ theme.lua
@@ -1,11 +1,16 @@
 local theme = {
-    bg_deep = Color3.fromRGB(5, 5, 5),       -- #050505
-    bg_sidebar = Color3.fromRGB(10, 0, 0),    -- #0a0000
-    bg_element = Color3.fromRGB(17, 17, 17),  -- #111111
-    accent_main = Color3.fromRGB(136, 0, 0),  -- #880000
-    accent_glow = Color3.fromRGB(255, 0, 0),  -- #ff0000
+    -- Renkler (HTML'deki Hex kodlarının karşılığı)
+    bg_deep = Color3.fromRGB(5, 5, 5),        -- #050505
+    bg_sidebar = Color3.fromRGB(10, 0, 0),     -- #0a0000
+    bg_element = Color3.fromRGB(17, 17, 17),   -- #111111
+    accent_main = Color3.fromRGB(136, 0, 0),   -- #880000
+    accent_glow = Color3.fromRGB(255, 0, 0),   -- #ff0000
     text_main = Color3.fromRGB(255, 255, 255),
     text_dark = Color3.fromRGB(136, 136, 136),
-    window_size = UDim2.new(0, 620, 0, 390)   --
+    border_color = Color3.fromRGB(42, 0, 0),   -- #2a0000
+    
+    -- Ayarlar
+    window_size = UDim2.new(0, 620, 0, 390),
+    transition_speed = 0.3 -- HTML'deki 0.3s cubic-bezier
 }
 return theme
