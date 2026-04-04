c52c5b3bb13446697eab47f44de0f8a1c688dc8a
Sole <adcbuyucu@gmail.com>
2026-04-04 08:05:10 +0300

    Update elements.lua

--- elements.lua
+++ elements.lua
@@ -1,17 +1,27 @@
-local theme = require("theme")
+local theme = _G.DragoTheme -- Loader'dan gelecek
 local Elements = {}
 
-function Elements.drawButton(x, y, text)
-    -- Kart (Arka plan)
-    love.graphics.setColor(0.1, 0.1, 0.1, 0.5)
-    love.graphics.rectangle("fill", x, y, 400, 45)
-    
-    -- Buton
-    love.graphics.setColor(theme.accent_main)
-    love.graphics.rectangle("fill", x + 280, y + 10, 100, 25)
-    
-    love.graphics.setColor(theme.text_main)
-    love.graphics.print(text, x + 10, y + 15)
+function Elements.CreateButton(parent, text, callback)
+    local btnFrame = Instance.new("Frame")
+    btnFrame.Size = UDim2.new(0, 400, 0, 45) --
+    btnFrame.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+    btnFrame.Parent = parent
+
+    local textLabel = Instance.new("TextLabel")
+    textLabel.Text = text
+    textLabel.Size = UDim2.new(0, 200, 1, 0)
+    textLabel.TextColor3 = theme.text_main
+    textLabel.BackgroundTransparency = 1
+    textLabel.Parent = btnFrame
+
+    local realBtn = Instance.new("TextButton")
+    realBtn.Size = UDim2.new(0, 100, 0, 25) --
+    realBtn.Position = UDim2.new(0, 280, 0, 10) --
+    realBtn.BackgroundColor3 = theme.accent_main
+    realBtn.Text = "Click"
+    realBtn.Parent = btnFrame
+
+    realBtn.MouseButton1Click:Connect(callback)
 end
 
 return Elements
